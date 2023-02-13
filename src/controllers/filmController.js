4700af0309380d2f2c9b09c0e4b759416e691c77
Adrian Dantas <230226+adriandantas@users.noreply.github.com>
2023-02-13 11:44:57 -0300

    Refactored handlers response to ensure json responses.

--- src/controllers/filmController.js
+++ src/controllers/filmController.js
@@ -11,18 +11,20 @@ function getFilm(payload) {
 
 async function findAll(req, res) {
   const films = await Film.findAll();
-  res.send(films);
+  res.json(films);
 }
 
 async function findById(req, res) {
   const { id } = req.params;
   if (id === null || id.trim() === '') {
-    return res.status(404).send('Missing ID parameter.');
+    return res.status(404).json({ message: 'Missing ID parameter.' });
   }
   const film = await Film.findById(req.params.id);
   if (!film)
-    return res.status(404).send('The film with the given ID was not found.');
-  return res.send(film);
+    return res
+      .status(404)
+      .json({ message: 'The film with the given ID was not found.' });
+  return res.json(film);
 }
 
 async function create(req, res) {
@@ -30,7 +32,7 @@ async function create(req, res) {
   if (error) return res.status(400).send(error.details[0].message);
   let film = getFilm(req.body);
   film = await Film.create(film);
-  return res.send(film);
+  return res.status(201).json(film);
 }
 
 async function update(req, res) {
@@ -39,8 +41,10 @@ async function update(req, res) {
   const data = getFilm(req.body);
   const film = await Film.update(data);
   if (!film)
-    return res.status(404).send('The film with the given ID was not found.');
-  return res.send(film);
+    return res
+      .status(404)
+      .json({ message: 'The film with the given ID was not found.' });
+  return res.json(film);
 }
 
 async function remove(req, res) {
@@ -50,8 +54,10 @@ async function remove(req, res) {
   }
   const film = await Film.remove(id);
   if (!film)
-    return res.status(404).send('The film with the given ID was not found.');
-  return res.send(film);
+    return res
+      .status(404)
+      .json({ message: 'The film with the given ID was not found.' });
+  return res.json(film);
 }
 
 module.exports = {
