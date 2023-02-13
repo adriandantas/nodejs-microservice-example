78930d9041a0f1debfc681de08897af68f59b716
Adrian Dantas <adrian.dantas@gmail.com>
2023-02-13 12:23:24 -0300

    Improved error message payload.

--- src/controllers/filmController.js
+++ src/controllers/filmController.js
@@ -1,3 +1,4 @@
+const { ValidationError } = require('joi');
 const Film = require('../models/filmRepository');
 
 function getFilm(payload) {
@@ -21,15 +22,27 @@ async function findById(req, res) {
   }
   const film = await Film.findById(req.params.id);
   if (!film)
-    return res
-      .status(404)
-      .json({ message: 'The film with the given ID was not found.' });
+    return res.status(404).json({
+      error: 'Not found',
+      message: 'The film with the given ID was not found.',
+    });
   return res.json(film);
 }
 
 async function create(req, res) {
   const { error } = Film.validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) {
+    // TODO include logger
+    if (error instanceof ValidationError)
+      return res.status(400).json({
+        error: 'Invalid request',
+        message: 'All fields are required',
+      });
+    return res.status(400).json({
+      error: 'Invalid request',
+      message: 'Payload could not be processed.',
+    });
+  }
   let film = getFilm(req.body);
   film = await Film.create(film);
   return res.status(201).json(film);
@@ -41,22 +54,27 @@ async function update(req, res) {
   const data = getFilm(req.body);
   const film = await Film.update(data);
   if (!film)
-    return res
-      .status(404)
-      .json({ message: 'The film with the given ID was not found.' });
+    return res.status(404).json({
+      error: 'Not found',
+      message: 'The film with the given ID was not found.',
+    });
   return res.json(film);
 }
 
 async function remove(req, res) {
   const { id } = req.params;
   if (id === null || id.trim() === '') {
-    return res.status(404).send('Missing ID parameter.');
+    return res.status(401).json({
+      error: 'Invalid request',
+      message: 'Missing ID parameter.',
+    });
   }
   const film = await Film.remove(id);
   if (!film)
-    return res
-      .status(404)
-      .json({ message: 'The film with the given ID was not found.' });
+    return res.status(404).json({
+      error: 'Not found',
+      message: 'The film with the given ID was not found.',
+    });
   return res.json(film);
 }
 
