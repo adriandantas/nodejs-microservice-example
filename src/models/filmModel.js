8820c6efc28e4b8fa327c81433bce11931886531
Adrian Dantas <adrian.dantas@gmail.com>
2023-02-13 07:36:56 -0300

    Refactored fixture data (#1)
    * Removed version keys from Film model since it is not required for this sample project.
    
    * Refactore Film fixture data to a json file.

--- src/models/filmModel.js
+++ src/models/filmModel.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
-const filmSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  year: { type: Number, required: true },
-  genre: { type: String, required: true },
-  director: { type: String, required: true },
-});
+const filmSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    year: { type: Number, required: true },
+    genre: { type: String, required: true },
+    director: { type: String, required: true },
+  },
+  {
+    collection: 'Film',
+    versionKey: false,
+  },
+);
 
 module.exports = mongoose.model('Film', filmSchema);
