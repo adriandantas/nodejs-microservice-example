36bd908c369cc6bf368db9ae9f32e5526f4a3e5e
Adrian Dantas <adrian.dantas@gmail.com>
2023-02-12 21:22:14 -0300

    Correction on import order.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+const mongoose = require('mongoose');
 const app = require('./src/app');
 const logger = require('./src/util/logger');
-const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
