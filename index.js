590eaa647606807dcbd16ba104aa963e735dd72f
Adrian Dantas <adrian.dantas@gmail.com>
2023-02-13 22:55:29 -0300

    MongoDB configuration tuning (#6)
    * Removed version keys from Film model since it is not required for this sample project.
    
    * Refactore Film fixture data to a json file.
    
    * Refactored FilModel constructor reference.
    
    * Included afterEach to clean up collection.
    
    * Renamed validation describe.
    
    * Added new Film creation tests.
    
    * Refactored FilmModel constructor name.
    
    * Custom error to agregate message and source Error.
    
    * Added database error handling on findById
    
    * Refactored handlers response to ensure json responses.
    
    * Improved error message payload.
    
    * Included logger in filmController.js
    
    * Completed refactoring of filmController.test.js
    
    * Correction on update function.
    
    * Pruned unused code.
    
    * Removed postman from project.
    
    * Silence logs on test environment.
    
    * Updated package-lock.json.
    
    * Removed unused validations.
    
    * Formatting.
    
    * MongoDB driver configuration to silence harmless warning about findAndModify deprecation.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const logger = require('./src/util/logger');
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 const port = process.env.PORT || 3000;
