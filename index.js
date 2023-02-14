8dd187681e8c1151024760baa92f20df2b94e275
Adrian Dantas <230226+adriandantas@users.noreply.github.com>
2023-02-14 07:54:43 -0300

    Included better logging on mongoose setup.

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 const app = require('./src/app');
 const logger = require('./src/util/logger');
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => logger.info({ message: 'Connected to MongoDB.' }))
+  .catch((error) =>
+    logger.error({ message: 'Failed to Connect to MongoDB.', source: error }),
+  );
 
 const port = process.env.PORT || 3000;
 
