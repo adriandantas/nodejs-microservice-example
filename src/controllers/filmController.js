3d167b9e8a15a0327ea03a936c22143c36d4cd49
Adrian Dantas <adrian.dantas@gmail.com>
2023-02-15 17:44:39 -0300

    Refactored code duplication on missing film id report code.

--- src/controllers/filmController.js
+++ src/controllers/filmController.js
@@ -1,6 +1,14 @@
 const Film = require('../models/filmRepository');
 const logger = require('../util/logger');
 
+function reportMissingFilmId(res) {
+  logger.error({ message: `The film with the given ID was not found.` });
+  return res.status(404).json({
+    error: 'Not found',
+    message: 'The film with the given ID was not found.',
+  });
+}
+
 function getFilm(payload) {
   return {
     title: payload.title,
@@ -20,11 +28,7 @@ async function findById(req, res) {
   const { id } = req.params;
   const film = await Film.findById(id);
   if (!film) {
-    logger.error({ message: `The film with the given ID was not found.` });
-    return res.status(404).json({
-      error: 'Not found',
-      message: 'The film with the given ID was not found.',
-    });
+    return reportMissingFilmId(res);
   }
   logger.info({ message: `Retrieved film with ID ${id}` });
   return res.json(film);
@@ -40,11 +44,7 @@ async function update(req, res) {
   const data = { _id: req.params.id, ...getFilm(req.body) };
   const film = await Film.update(data);
   if (!film) {
-    logger.error({ message: `The film with the given ID was not found.` });
-    return res.status(404).json({
-      error: 'Not found',
-      message: 'The film with the given ID was not found.',
-    });
+    return reportMissingFilmId(res);
   }
   logger.info({ message: `Updated film with ID ${film.id}` });
   return res.json(film);
@@ -54,11 +54,7 @@ async function remove(req, res) {
   const { id } = req.params;
   const film = await Film.remove(id);
   if (!film) {
-    logger.error({ message: `The film with the given ID was not found.` });
-    return res.status(404).json({
-      error: 'Not found',
-      message: 'The film with the given ID was not found.',
-    });
+    return reportMissingFilmId(res);
   }
   logger.info({ message: `Deleted film with ID ${film.id}` });
   return res.status(204).send();
