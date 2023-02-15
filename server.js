03bec7034004e93d555f8e4d4f8649653b9656a9
Adrian Dantas <adrian.dantas@gmail.com>
2023-02-15 11:02:48 -0300

    MongoDB version 6.9.1 upgrade (#9)
    * Upgraded mongoose to version 6.9.1
    
    * Removed deprecated useFindAndModify option.

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
   })
   .then(() => logger.info({ message: 'Connected to MongoDB.' }))
   .catch((error) =>
