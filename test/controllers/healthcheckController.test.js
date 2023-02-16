3835572ed5ed7a24317dfe4c7ae051dbe6d4f024
Adrian Dantas <adrian.dantas@gmail.com>
2023-02-16 09:18:59 -0300

    Added healthcheck route, controller and unit tests.

---
+++ test/controllers/healthcheckController.test.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const request = require('supertest');
+const { healthCheck } = require('../../src/controllers/healthcheckController');
+
+const app = express();
+app.use(express.json());
+
+app.get('/healthcheck', healthCheck);
+
+describe('healthcheckController', () => {
+  it('should return a 200 status code for GET /healthcheck', async () => {
+    const response = await request(app).get('/healthcheck');
+    expect(response.statusCode).toEqual(200);
+  });
+
+  it('should return a JSON object with "status" and "timestamp" properties for GET /healthcheck', async () => {
+    const response = await request(app).get('/healthcheck');
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        status: expect.any(String),
+        timestamp: expect.any(String),
+        uptime: expect.any(Number),
+        memoryUsage: expect.objectContaining({
+          rss: expect.any(Number),
+          heapTotal: expect.any(Number),
+          heapUsed: expect.any(Number),
+          external: expect.any(Number),
+        }),
+        env: expect.any(String),
+      }),
+    );
+  });
+});
