57a594b8bbc2a78c4ddaaebce57db43ca080cfb5
Adrian Dantas <230226+adriandantas@users.noreply.github.com>
2023-02-16 11:45:49 -0300

    Added graceful shutdown. (#12)

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const db = require('./src/config/db');
 db.init();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   logger.info(`listening on port ${port}`);
+});
+
+process.on('SIGTERM', () => {
+  logger.info({
+    message: 'SIGTERM signal received: closing microservice server',
+  });
+  server.close(() => {
+    logger.info({ message: 'Microservice server closed' });
+  });
+});
+
+process.on('SIGINT', () => {
+  logger.info({
+    message: 'SIGINT signal received: closing microservice server',
+  });
+  server.close(() => {
+    logger.info({ message: 'Microservice server closed' });
+  });
 });
