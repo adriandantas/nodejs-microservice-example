994283fc4c0257e6871f5d54228e4c5ca04babe2
Adrian Dantas <230226+adriandantas@users.noreply.github.com>
2023-02-19 14:05:44 -0300

    Add reques param id validation using middleware.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const { validateFilm } = require('./middlewares/validationMdw');
+const { validateFilm, validateId } = require('./middlewares/validationMdw');
 const filmController = require('./controllers/filmController');
 const { healthCheck } = require('./controllers/healthcheckController');
 
@@ -12,9 +12,9 @@ app.use(express.json());
 app.get('/healthcheck', healthCheck);
 
 app.get('/films', filmController.findAll);
-app.get('/films/:id', filmController.findById);
+app.get('/films/:id', validateId, filmController.findById);
 app.post('/films', validateFilm, filmController.create);
-app.put('/films/:id', validateFilm, filmController.update);
-app.delete('/films/:id', filmController.remove);
+app.put('/films/:id', validateId, validateFilm, filmController.update);
+app.delete('/films/:id', validateId, filmController.remove);
 
 module.exports = app;
