af3d13a73ef653a5fa0a0e32e8d28af4eb0e23e1
Adrian Dantas <adrian.dantas@gmail.com>
2023-02-19 14:04:46 -0300

    Added validationId middleware.

--- test/middlewares/validationMdw.test.js
+++ test/middlewares/validationMdw.test.js
@@ -1,5 +1,46 @@
 const filmsFixture = require('../fixtures/films.json');
-const { validateFilm } = require('../../src/middlewares/validationMdw');
+const {
+  validateFilm,
+  validateId,
+} = require('../../src/middlewares/validationMdw');
+
+describe('validateid', () => {
+  let req;
+  let res;
+  let next;
+
+  beforeEach(() => {
+    req = { headers: {}, params: {} };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+      json: jest.fn(),
+    };
+    next = jest.fn();
+  });
+
+  afterEach(() => {
+    req = null;
+    res = null;
+    next = null;
+  });
+
+  it('rejects values that do not validate as mongoose ObjectId', () => {
+    req.params.id = 'non_existent';
+    validateId(req, res, next);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalled();
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  it('Accepts valid 24 character hex value', () => {
+    req.params.id = '62261a65d66c6be0a63c051f';
+    validateId(req, res, next);
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+    expect(next).toHaveBeenCalled();
+  });
+});
 
 describe('validateFilm', () => {
   let filmData = {};
