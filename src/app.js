377614ee7894a831404cde8a69a382510dee0a65
Adrian Dantas <230226+adriandantas@users.noreply.github.com>
2023-02-20 14:00:08 -0300

    Swagger (#14)
    * Correction of static documentation html generation.
    
    * Correction of package version and description.
    
    * Added swagger-jsdoc dependency.
    
    * Refactored API path to /api/films instead of /films
    
    * Removed default value for env.
    
    * Added swagger definition for paths.
    
    * Included task to generate swagger documentation.
    
    * Added generated swagger documentation for the api.
    
    * Formatting correction.
    
    * Correction of invalid html structure.
    
    * Removal of commented code.
    
    * Inclusion of swagger-jsdoc as a package dependency.
    
    * Source code formatting correction.

--- src/app.js
+++ src/app.js
@@ -9,12 +9,179 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 
-app.get('/healthcheck', healthCheck);
+/**
+ * @swagger
+ * definitions:
+ *   Film:
+ *     type: object
+ *     properties:
+ *       title:
+ *         type: string
+ *         description: Title of the film
+ *       director:
+ *         type: string
+ *         description: Director of the film
+ *       year:
+ *         type: integer
+ *         minimum: 1800
+ *         description: Release year of the film
+ *       genre:
+ *         type: string
+ *         description: Genre of the film
+ *   Healthcheck:
+ *     type: object
+ *     properties:
+ *       status:
+ *         type: string
+ *         description: The status of the service, either OK or ERROR
+ *         example: "OK"
+ *       timestamp:
+ *         type: string
+ *         format: date-time
+ *         description: The current timestamp of the request
+ *         example: "1970-01-01T00:00:00.000Z"
+ *       uptime:
+ *         type: number
+ *         description: The uptime of the service in milliseconds
+ *         example: 123456789
+ *       memoryUsage:
+ *         type: object
+ *         description: The memory usage of the service
+ *       env:
+ *         type: string
+ *         description: The environment of the service, either "development" or "production"
+ *         example: "production"
+ */
 
-app.get('/films', filmController.findAll);
-app.get('/films/:id', validateId, filmController.findById);
-app.post('/films', validateFilm, filmController.create);
-app.put('/films/:id', validateId, validateFilm, filmController.update);
-app.delete('/films/:id', validateId, filmController.remove);
+/**
+ * @swagger
+ * /api/healthcheck:
+ *   get:
+ *     description: Healthcheck endpoint to check the status of the service
+ *     tags:
+ *       - Healthcheck
+ *     responses:
+ *       200:
+ *         description: A JSON object containing the status, timestamp, uptime, memory usage, and environment of the service
+ *         schema:
+ *           $ref: '#/definitions/Healthcheck'
+ */
+app.get('/api/healthcheck', healthCheck);
+
+/**
+ * @swagger
+ * /api/films:
+ *   get:
+ *     description: Retrieve all films
+ *     tags:
+ *       - Films
+ *     responses:
+ *       200:
+ *         description: An array of films
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/Film'
+ */
+app.get('/api/films', filmController.findAll);
+
+/**
+ * @swagger
+ * /api/films/{id}:
+ *   get:
+ *     description: Retrieve a film by id
+ *     tags:
+ *       - Films
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: Film id
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Film details
+ *         schema:
+ *           $ref: '#/definitions/Film'
+ *       404:
+ *         description: Film not found
+ */
+app.get('/api/films/:id', validateId, filmController.findById);
+
+/**
+ * @swagger
+ * /api/films:
+ *   post:
+ *     description: Create a new film
+ *     tags:
+ *       - Films
+ *     parameters:
+ *       - in: body
+ *         name: film
+ *         description: Film object that needs to be created
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Film'
+ *     responses:
+ *       201:
+ *         description: Film created successfully
+ *         schema:
+ *           $ref: '#/definitions/Film'
+ *       400:
+ *         description: Invalid request
+ */
+app.post('/api/films', validateFilm, filmController.create);
+
+/**
+ * @swagger
+ * /api/films/{id}:
+ *   put:
+ *     description: Update a film by id
+ *     tags:
+ *       - Films
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: Film id
+ *         required: true
+ *         type: string
+ *       - in: body
+ *         name: film
+ *         description: Film data to be updated
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Film'
+ *     responses:
+ *       200:
+ *         description: Film updated successfully
+ *         schema:
+ *           $ref: '#/definitions/Film'
+ *       400:
+ *         description: Invalid request
+ *       404:
+ *         description: Film not found
+ */
+app.put('/api/films/:id', validateId, validateFilm, filmController.update);
+
+/**
+ * @swagger
+ * /api/films/{id}:
+ *   delete:
+ *     description: Delete a film by id
+ *     tags:
+ *       - Films
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: Film id
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Film deleted successfully
+ *       404:
+ *         description: Film not found
+ */
+app.delete('/api/films/:id', validateId, filmController.remove);
 
 module.exports = app;
