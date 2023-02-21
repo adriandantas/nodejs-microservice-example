9cefa8389d1b05a47d026887d1832f3388f00896
Adrian Dantas <230226+adriandantas@users.noreply.github.com>
2023-02-21 11:48:23 -0300

    Refactored duplicated server termination handler. (#16)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 const logger = require('./src/util/logger');
 const db = require('./src/config/db');
+const terminationHandler = require('./src/util/terminationHandler');
 
 db.init();
 const port = process.env.PORT || 3000;
@@ -9,20 +10,5 @@ const server = app.listen(port, () => {
   logger.info(`listening on port ${port}`);
 });
 
-process.on('SIGTERM', () => {
-  logger.info({
-    message: 'SIGTERM signal received: closing microservice server',
-  });
-  server.close(() => {
-    logger.info({ message: 'Microservice server closed' });
-  });
-});
-
-process.on('SIGINT', () => {
-  logger.info({
-    message: 'SIGINT signal received: closing microservice server',
-  });
-  server.close(() => {
-    logger.info({ message: 'Microservice server closed' });
-  });
-});
+terminationHandler('SIGTERM', server);
+terminationHandler('SIGINT', server);
